329c2f1fc04077db7daf6a44af52e28ed91aa88d
Lê Văn Hoàng <90491910+hptc23@users.noreply.github.com>
2022-01-14 09:16:54 +0700

    Update Bai2.py

--- Bai2.py
+++ Bai2.py
@@ -1,11 +1,8 @@
-giatri1 = input() 
-giatri2 = input() 
+a=(input("Nhập tên đệm: "))
+b = (input("Nhập tên: "))
+c = a + b
+print(str(len(c)))
 
-try: 
- so1 = int(giatri1) 
- so2 = int(giatri2) 
- tong = so1 + so2 
- print("LE VAN HOANG")
- print("tong hai so la: ", tong) 
-except: 
- print("dinh dang dau vao khong hop le!") 
+
+a = (input ("Nhập tên: "))
+print (str (len (a)))
